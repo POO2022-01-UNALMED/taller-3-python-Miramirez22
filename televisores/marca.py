a33d04f712201799b62c201557768e18eb4ce6c3
Miramirez22 <miramirez@unal.edu.co>
2022-03-28 19:10:49 -0500

    Create marca.py

---
+++ televisores/marca.py
@@ -0,0 +1,9 @@
+class Marca:
+    def __init__(self,nombre):
+        self._nombre=nombre
+    
+    def setMarca(self,marca):
+        self._nombre(marca)
+    
+    def getMarca(self):
+        return self._nombre
